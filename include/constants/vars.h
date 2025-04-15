8fb18eb97101fa3c412946edf257e68f3b79225a
flmstrmz <flmstrmz@gmail.com>
2025-04-15 19:26:30 +0100

    ok

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -124,7 +124,7 @@
 #define VAR_ROUTE109_RH                                  0x4068
 #define VAR_ROUTE110_STATE                               0x4069
 #define VAR_ROUTE109_LC                                  0x406A
-#define VAR_ROUTE112_STATE                               0x406B // Unused Var
+#define VAR_ROUTE111_MEGA                                0x406B 
 #define VAR_ROUTE113_STATE                               0x406C // Unused Var
 #define VAR_ROUTE114_STATE                               0x406D // Unused Var
 #define VAR_ROUTE115_STATE                               0x406E // Unused Var
